01e837d200ae20ab9609bf6466f08f56d82927b4
Arsenull <29872263+Arsenull@users.noreply.github.com>
2019-05-21 23:17:01 +0300

    Initial

---
+++ install-Grafana.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+Colour='\033[1;31m'
+Color_Off='\033[0m'
+
+#get updates
+echo -e "${Colour}Checking for updates \n\n${Color_Off}"
+sudo apt-get update
+sudo apt-get upgrade
+
+#install Grafana
+echo -e "${Colour}Installing Grafana \n${Color_Off}"
+##wget https://dl.grafana.com/oss/release/grafana_6.1.6_armhf.deb
+##sudo dpkg -i grafana_6.1.6_armhf.deb
+
+#install Grafana plugins
+echo -e "${Colour}Installing Grafana plugins \n${Color_Off}"
+##grafana-cli plugins install ntop-ntopng-datasource
+##sudo grafana-cli plugins install ntop-ntopng-datasource
+grafana-cli plugins install grafana-clock-panel
+service grafana-server restart
+
+
+
+echo -e "\033[1;37m \n\nCreated by https://github.com/Arsenull\n${Color_Off}"
